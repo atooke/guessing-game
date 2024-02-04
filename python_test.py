9704289c4125c9db9dcefb0ad1aa02e85b7b86a4
Andy Tooke <andy.tooke@mckesson.com>
2024-02-04 13:52:18 -0500

    init

---
+++ python_test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def main():
+    print("hello friend")
+    df = pd.read_csv("main.rs")
+    x = x + 1
+    y = x + 2
+    print("hui")
+    x = x + 1
+    df = pd.read_csv("test.csv")
+    df.shape
