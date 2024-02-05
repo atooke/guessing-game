5f3b2586a4ea9ac6ffce244040c59cd8cb775896
Andy Tooke <andy.tooke@mckesson.com>
2024-02-05 07:24:47 -0500

    add match to check for valid entry

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,11 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("please type a number!");
+        //match returns Result enum that has OK/Err variants
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("you guessed {guess}");
 
